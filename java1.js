44afe24d87e3dfb69c4dead7c97cfcf2deadb387
sumicheeli <125802198+sumicheeli@users.noreply.github.com>
2023-05-31 14:47:23 +0530

    int commit

---
+++ java1.js
@@ -0,0 +1,41 @@
+/*let name="divya"
+let age=18
+document.write(name+"<br>")
+document.write(age+"<br>")
+name="sushma"
+document.write(name+"<br>")
+const clg="cbit"
+document.write(clg+"<br>")
+var name="sushma"
+function js(){
+    var age=18
+    document.write(name+"<br>")
+    document.write(age)
+}
+js()
+     document.write(name+"<br>")*/
+    /* var std={
+        nam:"sushma",
+        rollno:458,
+        emailid:"sushmareddynarala@gmail.com"
+    } 
+    for(let x in std){
+    document.write(std[x] + "<br>") 
+    }
+     document.write(std.nam+"<br>")
+     document.write(std.rollno+"<br>")
+     document.write(std.emailid+"<br>")
+     for( let x=0;x<5;x++){
+        document.write("2 *"+ x + "=" + (2*x) +"<br>")
+     }*/
+     let age=18
+     if(age>18){
+        document.write("your age is greater thaan 18"+"<br>")
+        document.write("your age =" + age)
+     }
+     else if(age==18){
+        document.write("your age is less than 18"+"<br>")
+     }
+     else {
+        document.write("your age is equal to 18")
+     }
